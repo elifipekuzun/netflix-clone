19de1376df4229134a0c9934c60b841882cc8b32
Elif  ipek <110392284+elifipekuzun@users.noreply.github.com>
2022-09-11 22:17:03 +0300

    adding page animation

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,10 +1,11 @@
 import React, { PropsWithChildren } from 'react';
 import { MainNavigation } from './main-navigation';
 import { Footer } from './footer';
+import styles from './layout.module.css';
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div style={{ overflowX: 'hidden' }}>
+    <div className={styles.layout} style={{ overflowX: 'hidden' }}>
       <MainNavigation />
       <main>{children}</main>
       <Footer />
---
+++ components/profile-gate-list/profile-gate-header.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren } from 'react';
+import Image from 'next/image';
+import styles from './profile-gate-header.module.css';
+
+export const ProfileGateHeader: React.FC<PropsWithChildren> = ({
+  children,
+}) => {
+  return (
+    <div>
+      <div className={styles.header}>
+        <div className={styles['logo-container']}>
+          <Image
+            src={'/images/logo/logo.png'}
+            alt="Netflix logo"
+            width={70}
+            height={29}
+          />
+        </div>
+      </div>
+      <div className={styles['profiles-gate-container']}>
+        <div className={styles['centered-profiles-container']}>{children}</div>
+      </div>
+    </div>
+  );
+};
